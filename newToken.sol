a8b54f87a8eaf087e5a42deeec66517793f9370c
Ashutoshagni003 <88612327+Ashutoshagni003@users.noreply.github.com>
2023-07-17 19:45:42 +0530

    Update newToken.sol

--- newToken.sol
+++ newToken.sol
@@ -2,19 +2,19 @@
 
 pragma solidity >=0.8.7;
 
-contract Simple {
+contract newToken {
  
 
     uint256 public number;
     string public message;
 
     function Number() external pure returns(uint){
-        return 1000;
+        return 1000*3;
         
     }
 
     function Message() external pure returns( string memory){
-        return "i am done with this";
+        return "I did it";
     }
 
 }
